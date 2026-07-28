d6a46b4c5171b304c698fa7a855eccc1f5e973f3
caohieeu <caohieeu2003@gmail.com>
2026-07-28 16:53:58 +0700

    add category selection

--- SkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ SkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SkyBookWeb.Application;
+using SkyBookWeb.Application.Common;
+using SkyBookWeb.Application.Implements;
+using SkyBookWeb.Core.Entities;
 using SkyBookWeb.Core.Specifications;
 
 namespace SkyBookWeb.Areas.Admin.Controllers
@@ -8,17 +13,50 @@ namespace SkyBookWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IProductService _productService;
-        public ProductController(IProductService productService)
+        private readonly ICategoryService _categoryService;
+        public ProductController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Create()
+        public async Task<IActionResult> Upsert()
         {
+            IEnumerable<SelectListItem> categoryList = (await _categoryService.GetCategoriesAsync())
+                .Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                });
+            ViewData["CategoryList"] = categoryList;
             return View();
         }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //[ActionName("Upsert")]
+        //public async Task<IActionResult> UpsertPost(Category category)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(category);
+        //    }
+
+        //    var result = await _productService.UpdateAsync(category);
+
+        //    if (result is ServiceResult<Category>.Failure failure)
+        //    {
+        //        ModelState.AddModelError("", failure.Errors);
+        //    }
+        //    else if (result is ServiceResult<Category>.Success success)
+        //    {
+        //        TempData["Success"] = "Update category successfully";
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(category);
+        //}
     }
 }
